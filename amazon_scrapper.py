4369286b1c407dc0a94c810ec10369e69491d79c
Jordan_Torres <jt.arontorres@gmail.com>
2020-10-19 10:05:03 -0700

    second commit

--- amazon_scrapper.py
+++ amazon_scrapper.py
@@ -3,3 +3,6 @@ from bs4 import BeautifulSoup as bs
 
 r = requests.get("https://www.ebay.com/itm/Kirkland-Signature-High-Performance-3-Piece-Wedge-Set-52-56-60/154144483938?hash=item23e3ba2a62:g:DYsAAOSwvvhfiLCH")
 
+soup = bs(r.content)
+
+print(soup)
