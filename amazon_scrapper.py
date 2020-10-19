53d1c9085db3f2cbecab0c4305eb38bdf2779b84
Jordan_Torres <jt.arontorres@gmail.com>
2020-10-19 10:46:58 -0700

    fourth commit, added second select to get the same line

--- amazon_scrapper.py
+++ amazon_scrapper.py
@@ -1,9 +1,15 @@
 import requests
 from bs4 import BeautifulSoup as bs
-
+#website listing
 r = requests.get("https://www.ebay.com/itm/Kirkland-Signature-High-Performance-3-Piece-Wedge-Set-52-56-60/154144483938?hash=item23e3ba2a62:g:DYsAAOSwvvhfiLCH")
-
+#content of the website in HTML
 soup = bs(r.content)
 
+#selects line of html 
 price = soup.select(".notranslate")
 print(price)
+
+#another way to find the link of html
+divs = soup.select('span#prcIsum')
+print(divs)
+
